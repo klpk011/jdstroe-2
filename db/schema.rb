64ccf69d4b5e013d852a432596e1c4b5115804f7
klpk011 <sophieQ011@gmail.com>
2017-02-08 20:38:48 +0800

    implement current_cart function

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 20170208115842) do
 
   create_table "cart_items", force: :cascade do |t|
-    t.integer  "card_id"
+    t.integer  "cart_id"
     t.integer  "product_id"
     t.integer  "quantity",   default: 1
     t.datetime "created_at",             null: false
